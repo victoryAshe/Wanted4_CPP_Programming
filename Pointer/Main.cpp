b072dc806d1c10792a027cad8b14640198d6f5c0
SuYeon <includeashe@gmail.com>
2026-01-21 09:59:17 +0900

    Pointer Memory Leak 검사 예제 진행
    - CRT Library를 활용해 Memory Leak 검사하는 예제 진행

--- Pointer/Main.cpp
+++ Pointer/Main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-void DeletePointer(void*& ptr)
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
+
+void DeletePointer(void* (&ptr))
 {
 	if (ptr != nullptr)
 	{
@@ -11,12 +23,18 @@ void DeletePointer(void*& ptr)
 
 int main()
 {
-	// 동적 할당
-	char* buffer = new char[10];
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
+	// TODO: Memory Leak 의도적으로 발생시키기
+	int* p = DBG_NEW int;
+
+	// 동적 할당
+	//char* buffer = new char[10];
 
 	void* pointer = new int;
 	DeletePointer(pointer);
+
+	delete(pointer);
 
 	if (pointer != nullptr)
 	{
